0e43eeb5729dd8a0af7e6c4ffdb725c0228d63ff
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2026-02-11 18:06:46 +0000

    feat(zero_trust_tunnel): create tunnel for windows 10 machine
    tunnel created to allow ollama ingress from localhost of windows 10 machine

---
+++ main.tf
@@ -0,0 +1,33 @@
+data "cloudflare_zone" "intuitive_judgments_dot_com_zone" {
+  filter = { name = "intuitive-judgments.com" }
+}
+
+resource "cloudflare_zero_trust_tunnel_cloudflared" "windows_10_localhost_zero_trust_tunnel" {
+  account_id = var.cloudflare_account_id
+  name       = "windows-10-tunnel"
+  config_src = "cloudflare"
+}
+
+
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "windows_10_localhost_zero_trust_tunnel_zero_trust_tunnel_config" {
+  # Warning: Resource Destruction Considerations
+  account_id = var.cloudflare_account_id
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.windows_10_localhost_zero_trust_tunnel.id
+  config = {
+    ingress = [{
+      hostname = "ollama.intuitive-judgments.com"
+      service  = "http://localhost:11434"
+      },
+      { service = "http_status:404" }
+    ]
+  }
+}
+
+resource "cloudflare_dns_record" "ollama_zero_trust_tunnel_dns" {
+  zone_id = data.cloudflare_zone.intuitive_judgments_dot_com_zone.id
+  name    = "ollama"
+  ttl     = "1"
+  type    = "CNAME"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.windows_10_localhost_zero_trust_tunnel.id}.cfargotunnel.com"
+  proxied = true
+}
