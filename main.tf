dbe6d9edcda6d86c28aa97e3d07e727c86856452
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2026-04-29 21:06:19 +0100

    feat(resend): add dns records for resend email sending
    added, mx (for bounce handling), SPF (TXT) and DMARC to dns records for `learneverything` subdomain.

--- main.tf
+++ main.tf
@@ -40,3 +40,39 @@ resource "cloudflare_dns_record" "ollama_zero_trust_tunnel_dns" {
   content = "${cloudflare_zero_trust_tunnel_cloudflared.windows_10_localhost_zero_trust_tunnel.id}.cfargotunnel.com"
   proxied = true
 }
+
+resource "cloudflare_dns_record" "resend_learneverything_dkim" {
+  zone_id = data.cloudflare_zone.intuitive_judgments_dot_com_zone.id
+  name    = "resend._domainkey.send"
+  type    = "TXT"
+  ttl     = 3600
+
+  content = "\"p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCXz69SUNQUTCFNtnleI7JCZu+W3wS+IUfgw26BTA1SksXaLBQO/1UXiLD7hbt2JA1bEc45QXS1hia3pi3Uf76JQok5DDMO5b0t3Xe418I12haQM5/N6b4NlaIDNnGsxemRWgzJ7yEJs/CaKNfss7eGeEyg2wnyqOWQTdcznquiEwIDAQAB\""
+}
+
+resource "cloudflare_dns_record" "resend_learneverything_mx" {
+  zone_id  = data.cloudflare_zone.intuitive_judgments_dot_com_zone.id
+  name     = "send"
+  type     = "MX"
+  ttl      = 3600
+  priority = 10
+  content  = "feedback-smtp.sa-east-1.amazonses.com"
+}
+
+resource "cloudflare_dns_record" "resend_learneverything_spf" {
+  zone_id = data.cloudflare_zone.intuitive_judgments_dot_com_zone.id
+  name    = "send"
+  type    = "TXT"
+  ttl     = 3600
+
+  content = "\"v=spf1 include:amazonses.com ~all\""
+}
+
+resource "cloudflare_dns_record" "resend_learneverything_dmarc" {
+  zone_id = data.cloudflare_zone.intuitive_judgments_dot_com_zone.id
+  name    = "_dmarc"
+  type    = "TXT"
+  ttl     = 3600
+
+  content = "\"v=DMARC1; p=none;\""
+}
